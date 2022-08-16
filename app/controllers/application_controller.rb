6554e080f7468619393f0caa2e1b6efcbf95aad1
kouji2004 <mrtaxi48@gmail.com>
2022-08-16 07:31:08 +0000

    post-image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ before_action :configure_permitted_parameters, if: :devise_controller?
     about_path
   end
 
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
   protected
 
   def configure_permitted_parameters
