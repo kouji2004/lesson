0ead021a07b5c28ad6ffe1c13ee4f54499c05dbc
kouji2004 <mrtaxi48@gmail.com>
2022-08-16 13:14:30 +0000

    post_image new

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,25 @@
+class PostImagesController < ApplicationController
+  def new
+    @post_image = PostImage.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
+  resources :post_images, only: [:new, :index, :show,:create]
   get 'homes/about' => 'homes#about',as:'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
