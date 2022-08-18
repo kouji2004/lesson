d51cd126f80ae90d78ed4b0ad45ec428e1c50929
kouji2004 <mrtaxi48@gmail.com>
2022-08-18 13:47:50 +0000

    post_image destroy

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,6 +18,12 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
   # 投稿データのストロングパラメータ
   private
 
