ef87efda04c32101fdf23ccd4ee6c4b93739bae6
kouji2004 <mrtaxi48@gmail.com>
2022-08-21 14:09:47 +0000

    indent 編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
-
 
   private
 
