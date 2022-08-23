81bba4c4cf22b5744f0fddf988a5c868fe93d87c
kouji2004 <mrtaxi48@gmail.com>
2022-08-23 08:40:04 +0000

    postimage_comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   has_one_attached :profile_image
 
