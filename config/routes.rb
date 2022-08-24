21679f0136046c88cc5e26070256703fc73236fa
kouji2004 <mrtaxi48@gmail.com>
2022-08-24 12:56:53 +0000

    favorite controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    has_many :favorites, dependent: :destroy
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 
